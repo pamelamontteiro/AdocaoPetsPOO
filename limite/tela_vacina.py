c2ee32cbb7fbcaa2d5ed9ddf360a5e85c8af9af6
thabatajohn <thabatajohn@gmail.com>
2024-12-11 17:06:43 -0300

    chore - implementação de interface para Vacina

---
+++ limite/tela_vacina.py
@@ -0,0 +1,41 @@
+import FreeSimpleGUI as sg
+
+class TelaVacina:
+    def __init__(self):
+        self.__window = None
+        self.__font = "Arial 14"
+
+    def pega_nome_vacina(self):
+        sg.theme("dark purple 5")
+        layout = [
+            [sg.Radio("Raiva", "Radio1", key="1", font=self.__font)],
+            [sg.Radio("Leptospirose", "Radio1", key="2", font=self.__font)],
+            [sg.Radio("Hepatite Infecciosa", "Radio1", key="3", font=self.__font)],
+            [sg.Push(), sg.Button("Retornar", button_color=("black", "white"), font=self.__font), sg.Push(), sg.Button("Confirmar", button_color=("black", "white"), font=self.__font)]
+        ]
+
+        self.__window = sg.Window("SELEÇ˜AO DE VACINA", layout, finalize=True)
+        self.__window.set_min_size((300, 200))
+
+        button, values = self.open()
+        if button in (None, "Retornar"):
+            nome_vacina = 0
+        elif values["1"]:
+            nome_vacina = 1
+        elif values["2"]:
+            nome_vacina = 2
+        elif values["3"]:
+            nome_vacina = 3
+        
+        self.close()
+        return nome_vacina
+    
+    def mensagem(self, mensagem):
+        sg.Popup("", mensagem)
+
+    def open(self):
+        button, values = self.__window.read()
+        return button, values
+    
+    def close(self):
+        self.__window.Close()
